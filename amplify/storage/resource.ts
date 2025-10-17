8e40833ec56c05b2ceed649848172a1f45227938
SMR-AIT <youremail@yourdomain.comsmrait@outlook.com>
2025-10-17 13:13:37 +0800

    update the storage

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,9 +1,7 @@
 import { defineStorage } from "@aws-amplify/backend";
-import { newsScraper } from "../news/news-scraper/resource";
-import { newsShortsCreator } from "../news/news-shorts-creator/resource";
 
 export const storage = defineStorage({
-  name: "languageAI",
+  name: "debug",
   access: (allow) => ({
     "public/*": [
       allow.authenticated.to(["read", "write", "delete"]),
@@ -16,12 +14,6 @@ export const storage = defineStorage({
     "private/snapshot/{entity_id}/*": [
       allow.entity("identity").to(["read", "write", "delete"]),
       allow.groups(["ADMINS"]).to(["read", "write", "delete"]),
-    ],
-    "news/*": [
-      allow.authenticated.to(["read", "write"]),
-      allow.groups(["ADMINS"]).to(["read", "write", "delete"]),
-      allow.resource(newsScraper).to(["write"]),
-      allow.resource(newsShortsCreator).to(["read", "write"]),
     ],
   }),
 });
